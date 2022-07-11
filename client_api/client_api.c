1a4293b5c42583c558aa28f899d5ddc4b60b56e1
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-07-11 19:25:01 +0530

    Created client api with dbus wrapper functions

--- client_api/client_api.c
+++ client_api/client_api.c
@@ -1,3 +1,62 @@
+#include "config_manager.h"
 #include <client_api.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+
+bool conf_client_init(conf_client *proxy, GError **err) {
+    bool success = true;
+    *proxy = 
+        org_regolith_config1_proxy_new_for_bus_sync( G_BUS_TYPE_SESSION, 
+                G_DBUS_PROXY_FLAGS_NONE,
+                "org.regolith.ConfigMgr", 
+                "/org/regolith/ConfigMgr",
+                NULL,
+                err);
+    if(*err) {
+        success = false;
+    } 
+    return success;
+}
+
+bool conf_client_load(conf_client proxy, char *file, bool nocpp, GError **err) {
+    return org_regolith_config1_call_load_sync(proxy, file, nocpp, NULL, err);
+}
+
+bool conf_client_load_cpp(conf_client proxy, char *file, char *cpp, GError **err) {
+    return org_regolith_config1_call_load_cpp_sync(proxy, file, cpp, NULL, err);
+}
+
+bool conf_client_merge(conf_client proxy, char *file, bool nocpp, GError **err) {
+    return org_regolith_config1_call_merge_sync(proxy, file, nocpp, NULL, err);
+}
+
+bool conf_client_merge_cpp(conf_client proxy, char *file, char *cpp, GError **err) {
+    return org_regolith_config1_call_merge_cpp_sync(proxy, file, cpp, NULL, err);
+}
+
+bool conf_client_query(conf_client proxy, char *query, char **result, GError **err) {
+    return org_regolith_config1_call_query_sync(proxy, query, result, NULL, err);
+}
+
+bool conf_client_get(conf_client proxy, char *key, char **result, GError **err) {
+    return org_regolith_config1_call_get_resource_sync(proxy, key, result, NULL, err);
+}
+
+bool conf_client_add_resource(conf_client proxy, char *key, char *value, GError **err) {
+    return org_regolith_config1_call_add_resource_sync(proxy, key, value, NULL, err);
+}
+
+bool conf_client_set_resource(conf_client proxy, char *key, char *value, GError **err) {
+    return org_regolith_config1_call_set_resource_sync(proxy, key, value, NULL, err);
+}
+
+bool conf_client_remove_all(conf_client proxy, GError **err) {
+    return org_regolith_config1_call_remove_all_sync(proxy, NULL, err);
+}
+
+bool conf_client_remove_one(conf_client proxy, char *key, GError **err) {
+
+    return org_regolith_config1_call_remove_one_sync(proxy,key ,NULL ,err);
+}
 
--- client_api/client_api.h
+++ client_api/client_api.h
@@ -1,2 +1,26 @@
+#ifndef REGOLITH_CONFIG_MANAGER_CLIENT_API_HEADER
+#define REGOLITH_CONFIG_MANAGER_CLIENT_API_HEADER
 #include <config_manager.h>
+#include <stdbool.h>
 
+typedef OrgRegolithConfig1 *conf_client;
+
+conf_client *proxy_new();
+bool conf_client_init(conf_client *proxy, GError **err);
+
+bool conf_client_load(conf_client proxy, char *file, bool nocpp, GError **err);
+bool conf_client_load_cpp(conf_client proxy, char *file, char *cpp, GError **err);
+
+bool conf_client_merge(conf_client proxy, char *file, bool nocpp, GError **err);
+bool conf_client_merge_cpp(conf_client proxy, char *file, char *cpp, GError **err);
+
+bool conf_client_query(conf_client proxy, char *query, char **result, GError **err);
+bool conf_client_get(conf_client proxy, char *key, char **result, GError **err);
+
+bool conf_client_add_resource(conf_client proxy, char *key, char *value, GError **err);
+bool conf_client_set_resource(conf_client proxy, char *key, char *value, GError **err);
+
+bool conf_client_remove_all(conf_client proxy, GError **err);
+bool conf_client_remove_one(conf_client proxy, char *key, GError **err);
+
+#endif //REGOLITH_CONFIG_MANAGER_CLIENT_API_HEADER
