265a9b09cec36d51173f57d2eeab53dfc3c7d985
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-07-15 16:02:14 +0530

    Updated postbuild script

--- postbuild.sh
+++ postbuild.sh
@@ -2,9 +2,17 @@
 # Use run this script after building trawld to generate updated 
 # Introspection xml for the dbus service
 
+./target/release/trawld &
+name_acquired=$?
+sleep 0.5
 dbus-send --session \
     --dest=org.regolith.Trawl \
     --type=method_call \
     --print-reply \
     /org/regolith/Trawl \
     org.freedesktop.DBus.Introspectable.Introspect | tail -n +3 | head -n -1 > ./client_api/service.xml
+
+# kill trawld if it was not running before
+if [ $name_acquired -eq 0 ]; then
+    killall trawld
+fi
